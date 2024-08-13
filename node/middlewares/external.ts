c3826bff9754b4fe7cc5ae1974c2863e018b45ca
velosov <viniciusveloso@poli.ufrj.br>
2024-08-12 23:57:44 -0300

    chore: logs

--- node/middlewares/external.ts
+++ node/middlewares/external.ts
@@ -1,13 +1,10 @@
 export async function external(ctx: Context) {
-    const { clients: { target } , vtex: { route: { params }} } = ctx
-    const { account, id } = params
-    console.log(account, id)
-    ctx.vtex.settings = { ...ctx.vtex.settings, target: { account, workspace: 'logs' } } //changing the context does not change an already instantiated client
+    const { clients: { target } , vtex: { logger, route: { params }} } = ctx
+    const { id } = params
+    
     try {
-        console.log(ctx.vtex.settings)
         const attempt = await target.internal(typeof(id) === "string" ? id : id[0] )
-        console.log(ctx.vtex.settings)
         ctx.body = attempt
-    } catch(e) { console.log(e) }
+    } catch(e) { logger.error(e); throw new Error(`${e}`) }
     return
 }
--- node/middlewares/internal.ts
+++ node/middlewares/internal.ts
@@ -1,17 +1,21 @@
+import { UserInputError } from "@vtex/api";
 
 export async function internal(ctx: Context, next: () => Promise<any>) {
-    const { clients: { masterdata }, vtex: { route: { params }} } = ctx
-    console.log(params)
-    const resp = await masterdata.searchDocuments({
-        dataEntity: "Logs",
-        fields: ["any"],
-        pagination: {
-            pageSize: 10,
-            page: 1
-        },
-        where: `criteria=${"TID?"}`
-    })
-    ctx.status = 200
-    ctx.body = resp
-    await next
+    const { clients: { masterdata }, vtex: { logger } } = ctx
+
+    try {
+        // Change for your own query; this example ain't supposed to work since you probably don't have such mocked records
+        const resp = await masterdata.searchDocuments({
+            dataEntity: "Logs",
+            fields: ["any"],
+            pagination: {
+                pageSize: 10,
+                page: 1
+            },
+            where: `criteria=${"TID?"}`
+        })
+        ctx.status = 200
+        ctx.body = resp
+    } catch(e) { logger.error(e); throw new UserInputError(`Invalid MD Search: ${e}`) }
+    await next()
 }
