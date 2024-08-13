18441ca1d52e462c7b568bfffe5f7239d3ac87be
velosov <viniciusveloso@poli.ufrj.br>
2024-08-12 23:57:03 -0300

    Pre-sets target client

--- node/middlewares/load.ts
+++ node/middlewares/load.ts
@@ -1,6 +1,8 @@
+// It's necessary to set such context before initializing Target client
+
 export async function load(ctx: Context, next: () => Promise<any>) {
     const { vtex: { route: { params }} } = ctx
     const { account, id } = params
-    ctx.vtex.settings = { ...ctx.vtex.settings, target: { account, workspace: 'logs' }, id } //changing the context does not change an already instantiated client
-    next
+    ctx.vtex.settings = { ...ctx.vtex.settings, target: { account, workspace: 'elsewhere' }, id }
+    await next()
 }
