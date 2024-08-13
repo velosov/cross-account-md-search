1d67ca780ad279ffdae200f65faa955a0abdc183
velosov <viniciusveloso@poli.ufrj.br>
2024-08-13 00:35:32 -0300

    Adds example of ID as criteria

--- node/middlewares/internal.ts
+++ node/middlewares/internal.ts
@@ -1,10 +1,10 @@
 import { UserInputError } from "@vtex/api";
 
 export async function internal(ctx: Context, next: () => Promise<any>) {
-    const { clients: { masterdata }, vtex: { logger } } = ctx
-
+    const { clients: { masterdata }, vtex: { logger, route: { params } } } = ctx
     try {
         // Change for your own query; this example ain't supposed to work since you probably don't have such mocked records
+        console.log(params.id) //ID of root curl's comes here and can be used as search criteria
         const resp = await masterdata.searchDocuments({
             dataEntity: "Logs",
             fields: ["any"],
